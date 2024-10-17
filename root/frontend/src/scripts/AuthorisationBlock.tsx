ff167c1df606cf55bae4f1ec29e051d1b2d30c10
EugeneTiutin <eugenevoice87@gmail.com>
2024-10-17 12:00:01 +0300

    Adding some design

--- root/frontend/src/scripts/AuthorisationBlock.tsx
+++ root/frontend/src/scripts/AuthorisationBlock.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import './AuthorisationBlock.css';
-
-type Props = {};
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Register from './Register';
+import MainHeader from "./MainHeader";
 
 function AuthorisationBlock()
 {
     return (
         <body>
             <div className="AuthorisationButtons">
+                {/* <Router>
+                    <Routes>
+                        <Route path="/"/>   
+                        <Route path="/Register" element={<Register/>} />
+                    </Routes>
+                </Router> */}
+
                 <button className="RegisterButton Button">
                     Зарегистрироваться
                 </button>
@@ -20,3 +28,5 @@ function AuthorisationBlock()
 }
 
 export default AuthorisationBlock;
+
+
---
+++ root/frontend/src/scripts/Register.tsx
@@ -0,0 +1,14 @@
+function Register() {
+    return (
+        <html lang="en">
+        <head>
+            <title>Document</title>
+        </head>
+        <body>
+            
+        </body>
+        </html>
+    );
+}
+
+export default Register;
